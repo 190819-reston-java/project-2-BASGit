946499487e30cc4a8d704a12500061e3b46a051f
Chris Allen <chris.allen070@gmail.com>
2019-10-08 15:41:31 -0400

    Trying to get all files uploaded to github

---
+++ hibernatePractice/src/main/java/com/revature/model/User.java
@@ -0,0 +1,52 @@
+package com.revature.model;
+
+public class User {
+
+	private String username;
+	private String email;
+	private String password;
+	private String avatarURL;
+	private String blurb;
+	
+	public User(){}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getAvatarURL() {
+		return avatarURL;
+	}
+
+	public void setAvatarURL(String avatarURL) {
+		this.avatarURL = avatarURL;
+	}
+
+	public String getBlurb() {
+		return blurb;
+	}
+
+	public void setBlurb(String blurb) {
+		this.blurb = blurb;
+	}
+}
