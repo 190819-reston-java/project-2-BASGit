38949bcb2ac8a7dd57db2d1ef0794d700100f82a
Borko Stankovic <borkostankovic83@yahoo.com>
2019-10-11 12:52:09 -0400

    Slight change

---
+++ BASGit/src/app/AppModule.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FooterComponent } from './footer/footer.component';
+import { NewsfeedComponent } from './newsfeed/newsfeed.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AccountDropdownComponent } from './account-dropdown/account-dropdown.component';
+import { TopNewsComponent } from './top-news/top-news.component';
+import { AppRoutingModule } from './app-routing.module';
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    FooterComponent,
+    NewsfeedComponent,
+    SidebarComponent,
+    NavbarComponent,
+    AccountDropdownComponent,
+    TopNewsComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
