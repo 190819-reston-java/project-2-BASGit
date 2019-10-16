52ba8b35029a4482f1d4c5ce569e26c93d1e7afa
JGworek <jonathangworek@gmail.com>
2019-10-16 13:22:51 -0400

    angular has different forms

--- BASGit/src/app/sign-up-page/sign-up-page.component.ts
+++ BASGit/src/app/sign-up-page/sign-up-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-sign-up-page',
@@ -7,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  
+  submit(formGroup)
+  {
+
+        this.http.post("http://ec2-52-90-209-187.compute-1.amazonaws.com:5555/BASGit/users/signup", formGroup.value).subscribe(res=>{
+                 //here you received the response of your post
+                 console.log(res);
+                 //you can do asomething, like
+           })
+  }
 
   ngOnInit() {
   }
