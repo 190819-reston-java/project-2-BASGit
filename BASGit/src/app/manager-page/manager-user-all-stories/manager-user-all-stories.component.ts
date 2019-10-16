57fdc7f4c0bd989877e10a17f9154b935d109b23
JGworek <jonathangworek@gmail.com>
2019-10-16 16:37:27 -0400

    all forms updated

--- BASGit/src/app/manager-page/manager-user-all-stories/manager-user-all-stories.component.ts
+++ BASGit/src/app/manager-page/manager-user-all-stories/manager-user-all-stories.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NgForm } from '@angular/forms'
 
 @Component({
   selector: 'app-manager-user-all-stories',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManagerUserAllStoriesComponent implements OnInit {
 
-  constructor() { }
+  stories: any;
 
-  ngOnInit() {
+  constructor(private http : HttpClient) { }
+
+  onSubmitH(f: NgForm) {
+    this.http.post("http://ec2-52-90-209-187.compute-1.amazonaws.com:5555/BASGit/stories/admin/handle", f.value).subscribe(res=>{
+                 //here you received the response of your post
+                 console.log(res);
+                 //you can do asomething, like
+           })
+    console.log(f.value);  // { first: '', last: '' }
+    console.log(f.valid);  // false
   }
 
+  onSubmitR(f: NgForm) {
+    this.http.post("http://ec2-52-90-209-187.compute-1.amazonaws.com:5555/BASGit/stories/admin/handle", f.value).subscribe(res=>{
+                 //here you received the response of your post
+                 console.log(res);
+                 //you can do asomething, like
+           })
+    console.log(f.value);  // { first: '', last: '' }
+    console.log(f.valid);  // false
+  }
+
+ngOnInit() {
+    let observable = this.http.get('http://ec2-52-90-209-187.compute-1.amazonaws.com:5555/BASGit/stories')
+    observable.subscribe((result => {
+      this.stories = result;
+    }))
+  }
 }
--- BASGit/src/app/profile-page/update-profile/update-profile.component.ts
+++ BASGit/src/app/profile-page/update-profile/update-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NgForm } from '@angular/forms'
 
 @Component({
   selector: 'app-update-profile',
@@ -7,7 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  onSubmit(f: NgForm) {
+    this.http.post("http://ec2-52-90-209-187.compute-1.amazonaws.com:5555/BASGit/users/update", f.value).subscribe(res=>{
+                 //here you received the response of your post
+                 console.log(res);
+                 //you can do asomething, like
+           })
+    console.log(f.value);  // { first: '', last: '' }
+    console.log(f.valid);  // false
+  }
 
   ngOnInit() {
   }
