1142101444d3a47af20fbd904b34c08edd6419a3
JGworek <jonathangworek@gmail.com>
2019-10-17 11:05:10 -0400

    setting up news submission link

--- BASGit/src/app/user-stories-page/submit-story/submit-story.component.ts
+++ BASGit/src/app/user-stories-page/submit-story/submit-story.component.ts
@@ -14,7 +14,7 @@ export class SubmitStoryComponent implements OnInit {
     private router: Router) { }
 
   onSubmit(f: NgForm) {
-    this.http.post("http://ec2-52-90-209-187.compute-1.amazonaws.com:5555/BASGit/test/storySubmission", JSON.stringify(f.value)).subscribe(res=>{
+    this.http.post("http://ec2-52-90-209-187.compute-1.amazonaws.com:5555/BASGit/stories/profiles/submit", JSON.stringify(f.value)).subscribe(res=>{
                  //here you received the response of your post
                  console.log(res);
                  //you can do asomething, like
