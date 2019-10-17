147e2fc7a7ddaca5ee7810da0bf5163eb2a32a1b
JGworek <jonathangworek@gmail.com>
2019-10-17 02:23:33 -0400

    angular changed click in update

--- BASGit/src/app/manager-page/manager-user-all-stories/manager-user-all-stories.component.ts
+++ BASGit/src/app/manager-page/manager-user-all-stories/manager-user-all-stories.component.ts
@@ -14,21 +14,21 @@ export class ManagerUserAllStoriesComponent implements OnInit {
   constructor(private http : HttpClient) { }
 
 highlightStory(id) {
-  this.http.post("http://ec2-52-90-209-187.compute-1.amazonaws.com:5555/BASGit/stories/admin/handle", id+" h").subscribe(res=>{
+  this.http.post("http://ec2-52-90-209-187.compute-1.amazonaws.com:5555/BASGit/stories/admin/handle", `${id} h`).subscribe(res=>{
     //here you received the response of your post
     console.log(res);
     //you can do asomething, like
 })
-console.log(id+" h");
+console.log(`${id} h`);
 }
 
 removeStory(id) {
-  this.http.post("http://ec2-52-90-209-187.compute-1.amazonaws.com:5555/BASGit/stories/admin/handle", id+" r").subscribe(res=>{
+  this.http.post("http://ec2-52-90-209-187.compute-1.amazonaws.com:5555/BASGit/stories/admin/handle", `${id} r`).subscribe(res=>{
     //here you received the response of your post
     console.log(res);
     //you can do asomething, like
 })
-console.log(id+" r");
+console.log(`${id} r`);
 }
 
 
