1998eeb944ee9b7b6a06cd77dad068e4c54d3258
JGworek <jonathangworek@gmail.com>
2019-10-16 21:24:14 -0400

    stringify

--- BASGit/src/app/sign-up-page/sign-up-page.component.ts
+++ BASGit/src/app/sign-up-page/sign-up-page.component.ts
@@ -14,7 +14,7 @@ export class SignUpPageComponent implements OnInit {
   constructor(private http: HttpClient) {}
 
    onSubmit(f: NgForm) {
-    this.http.post("http://ec2-52-90-209-187.compute-1.amazonaws.com:5555/BASGit/test/formSubmission", f.value).subscribe(res=>{
+    this.http.post("http://ec2-52-90-209-187.compute-1.amazonaws.com:5555/BASGit/test/formSubmission", JSON.stringify(f.value)).subscribe(res=>{
                  //here you received the response of your post
                  console.log(res);
                  //you can do asomething, like
