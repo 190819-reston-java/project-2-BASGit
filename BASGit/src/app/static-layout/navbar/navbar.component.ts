ce9e75a552f86d165bb77d64da188e36ebde8d8c
JGworek <jonathangworek@gmail.com>
2019-10-16 23:46:42 -0400

    conditional stuff

--- BASGit/src/app/static-layout/navbar/navbar.component.ts
+++ BASGit/src/app/static-layout/navbar/navbar.component.ts
@@ -10,6 +10,8 @@ import { NgForm } from '@angular/forms'
 })
 export class NavbarComponent implements OnInit {
 
+  loggedInUser: any = null;
+
   constructor(private http: HttpClient) { }
 
   //HEY, MAKE SURE TO CHECK THIS ENDPOINT
@@ -22,7 +24,12 @@ export class NavbarComponent implements OnInit {
            })
   }
 
-  ngOnInit() {
+    ngOnInit() {
+    let observable = this.http.get('http://ec2-52-90-209-187.compute-1.amazonaws.com:5555/BASGit/users/current')
+    observable.subscribe((result => {
+      this.loggedInUser = result;
+    }))
+
   }
 
 }
--- BASGit/src/app/user-stories-page/sidebar-user-news/sidebar-user-news.component.ts
+++ BASGit/src/app/user-stories-page/sidebar-user-news/sidebar-user-news.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NgForm } from '@angular/forms'
 
 @Component({
   selector: 'app-sidebar-user-news',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarUserNewsComponent implements OnInit {
 
-  constructor() { }
+  loggedInUser: any = null;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    let observable = this.http.get('http://ec2-52-90-209-187.compute-1.amazonaws.com:5555/BASGit/users/current')
+    observable.subscribe((result => {
+      this.loggedInUser = result;
+    }))
   }
 
 }
