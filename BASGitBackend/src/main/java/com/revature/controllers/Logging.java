421e68f9a78a79d729f191a3582a98e02c75d728
Bryan <BryanFRitt@HotMail.com>
2019-10-16 22:30:20 -0400

    This is me just saying I started looking into this. For sample code see: training-code/week6/SpringAOPDemo/src/main/java/com/revature/aspects/

---
+++ BASGitBackend/src/main/java/com/revature/controllers/Logging.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package com.revature.controllers;
+
+import org.apache.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+// see: training-code/week6/SpringAOPDemo/src/main/java/com/revature/aspects/
+
+@Aspect
+@Component
+public class Logging {
+	
+		private static final Logger storyControllerLog = Logger.getLogger(Logging.class);
+		
+		//@Before("within(com.revature.controllers.StoryController)")
+		@Before("execution(* *(..))")
+		public static void logUserMethods(JoinPoint jp) {
+			storyControllerLog.info(jp.getTarget() + " has invoked " + jp.getSignature());
+			System.out.println("Hi?");
+		}
+			
+//	public Logging() {
+//	}
+
+//	/**
+//	 * @param args
+//	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println(StoryController.testingString());
+		StoryController.testingString();		
+	}
+
+}
+
