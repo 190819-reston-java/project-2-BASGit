495c1f4c34f657a613a7c55fb6e7c179d89b31a1
Chris Allen <chris.allen070@gmail.com>
2019-10-16 21:11:09 -0400

    Added test servlet

--- BASGitBackend/src/main/java/com/revature/controllers/HelloWorldController.java
+++ BASGitBackend/src/main/java/com/revature/controllers/HelloWorldController.java
@@ -3,6 +3,8 @@ package com.revature.controllers;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -14,5 +16,9 @@ public class HelloWorldController {
 		return new ResponseEntity<String>("Hello!", HttpStatus.OK);
 	}
 	
+	@PostMapping(value="/test/formSubmission")
+	public ResponseEntity<String> buildForm(@RequestBody String JSON){
+		return ResponseEntity.status(HttpStatus.OK).body(JSON);
+	}
 	
 }
